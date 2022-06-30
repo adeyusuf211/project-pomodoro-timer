887981cd0ed5061abadd34bde5325e170b7f4155
Ade Yusuf <adeyusuf005@gmail.com>
2022-06-30 15:47:03 +0700

    Melakukan sedikit perbaikan

--- script.js
+++ script.js
@@ -10,56 +10,60 @@ var pomodoro = {
     var self = this;
     this.minutesDom = document.querySelector("#min");
     this.secondsDom = document.querySelector("#sec");
-
     this.interval = setInterval(function () {
       self.intervalCallback.apply(self);
     }, 1000);
-
     document.querySelector("#start").onclick = function () {
       self.startWork.apply(self);
+    };
+    document.querySelector("#shortBreak").onclick = function () {
+      self.startShortBreak.apply(self);
+    };
+    document.querySelector("#longBreak").onclick = function () {
+      self.startLongBreak.apply(self);
     };
     document.querySelector("#stop").onclick = function () {
       self.stopTimer.apply(self);
     };
   },
-
   resetVariables: function (mins, secs, started) {
     this.minutes = mins;
     this.seconds = secs;
     this.started = started;
     this.fillerIncrement = 200 / (this.minutes * 60);
   },
-
   startWork: function () {
-    this.resetVariables(25, 00, true);
+    this.resetVariables(25, 0, true);
   },
-
+  startShortBreak: function () {
+    this.resetVariables(5, 0, true);
+  },
+  startLongBreak: function () {
+    this.resetVariables(15, 0, true);
+  },
   stopTimer: function () {
     this.resetVariables(25, 0, false);
     this.updateDom();
   },
-
   toDoubleDigit: function (num) {
     if (num < 10) {
       return "0" + parseInt(num, 10);
     }
     return num;
   },
-
   updateDom: function () {
     this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
     this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
   },
-
   intervalCallback: function () {
     if (!this.started) return false;
     if (this.seconds == 0) {
       if (this.minutes == 0) {
         this.timerComplete();
 
-        const music = new Audio("/stomping-rock-four-shots-111444.mp3");
+        const music = new Audio("audio.mp3");
         music.play();
-        music.loop = false;
+        music.loop = true;
         music.playbackRate = 2;
 
         return;
@@ -80,3 +84,10 @@ var pomodoro = {
 window.onload = function () {
   pomodoro.init();
 };
+
+const boxs = document.querySelectorAll(".box");
+boxs.forEach((box) => {
+  box.addEventListener("click", function () {
+    document.body.style.backgroundColor = box.textContent;
+  });
+});
